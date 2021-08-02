e468dfe94c5bdbf055d493e76884e40227651843
mateo <mateo.zikovic3@gmail.com>
2021-08-02 12:40:54 +0200

    added main and index .hbs, app tested and running

---
+++ server/app.js
@@ -0,0 +1,71 @@
+const express           = require('express');
+const bodyParser        = require('body-parser');
+const cors              = require('cors');
+const hbs               = require('express-handlebars');
+const session           = require('express-session');
+const mongoose          = require('mongoose');
+const passport          = require('passport');
+const localStrategy     = require('passport-local').Strategy;
+const bcrypt            = require('bcrypt'); 
+const app               = express();
+
+mongoose.connect("mongodb+srv://abc123:kikiriki@shiftr.wvvsa.mongodb.net/Shiftr?retryWrites=true&w=majority",
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.engine('hbs', hbs({ extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use(session({
+    secret: "verygoodsecret", 
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(express.urlencoded({ extended:false }));
+app.use(express.json());
+
+// Passport.js
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done){
+    User.findById(id, function(err, user) {
+        done(err, user)
+    })
+});
+
+passport.use(new localStrategy(function (username, password, done){
+    User.findOne({username: username}, function(err, user){
+        if (err) return done(err);
+        if (!user) return done(null, false, { message: 'Incorrect username.' }); 
+
+        bcrypt.compare(password, user.password, function (err, res) {
+            if (err) return done(err);
+            if (res === false) return done(null, false, {message: 'Incorrect password.'});
+            
+            return done(null, user);
+        });
+    });
+}));
+
+app.get('/', (req, res) => {
+    res.render("index", {title: "Home"});
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on ${port}`));
+
+
+

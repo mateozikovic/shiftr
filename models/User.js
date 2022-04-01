8a4cbe2a90071c479d96cbead96282bcc42ff9b6
Mateo <mateo.zikovic3@gmail.com>
2022-04-01 17:47:22 +0200

    cleanup

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Create the User Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  coworkers: {
+    type: String,
+    required: false
+  }
+});
+
+module.exports = User = mongoose.model("users", UserSchema);

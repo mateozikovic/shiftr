cf7ae9bcc9d9b349f5b60576428478dea0adf587
Mateo <mateo.zikovic3@gmail.com>
2022-04-10 01:50:12 +0200

    form

--- client/src/components/UI/AddShift.vue
+++ client/src/components/UI/AddShift.vue
@@ -1,8 +1,45 @@
 <template>
   <div>
     <h2>{{ workweekName }}</h2>
-    <div>{{ selectedWorkers }}</div>
-    <form></form>
+    <div>enter shift hours:</div>
+    <form>
+      <ul v-for="workers in selectedWorkers" :key="workers.id">
+        <li>
+          {{ workers.name }}
+          <ul>
+            <li>
+              <label for="monday">Monday</label>
+              <input type="text" name="monday" id="monday" />
+            </li>
+            <li>
+              <label for="monday">Tuesday</label>
+              <input type="text" name="tuesday" id="tuesday" />
+            </li>
+            <li>
+              <label for="monday">Wednesday</label>
+              <input type="text" name="wednesday" id="wednesday" />
+            </li>
+            <li>
+              <label for="monday">Thursday</label>
+              <input type="text" name="thursday" id="thursday" />
+            </li>
+            <li>
+              <label for="monday">Friday</label>
+              <input type="text" name="friday" id="friday" />
+            </li>
+            <li>
+              <label for="monday">Saturday</label>
+              <input type="text" name="saturday" id="saturday" />
+            </li>
+            <li>
+              <label for="monday">Sunday</label>
+              <input type="text" name="sunday" id="sunday" />
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <button>create</button>
+    </form>
   </div>
 </template>
 
@@ -11,3 +48,9 @@ export default {
   props: ["workweekName", "selectedWorkers"],
 };
 </script>
+
+<style scoped>
+li {
+  list-style: none;
+}
+</style>

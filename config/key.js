f60fea4d892a14b23e1057d37e9c39a5f83d341f
Mateo <mateo.zikovic3@gmail.com>
2022-04-11 03:47:35 +0200

    heroku deploy

--- config/key.js
+++ config/key.js
@@ -1,7 +1,10 @@
 // .env file variables
 require("dotenv").config();
-let db_user = process.env.DB_USER;
-let db_pass = process.env.DB_PASS;
+// let db_user = process.env.DB_USER;
+// let db_pass = process.env.DB_PASS;
+
+let db_user = "shiftr_app";
+let db_pass = "kikiriki"
 let key = "secretkey";
 
 // Export db connection URL and secret key
